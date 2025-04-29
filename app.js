3e49f12db7e77fad9288380847c9aecfbb7ec129
Aun-Ali-12 <aunali29331@gmail.com>
2025-04-29 23:17:50 +0500

    done

---
+++ app.js
@@ -0,0 +1,21 @@
+const nameArr = ["aun", "faizan", "kashan"];
+
+for (let i = 0; i < nameArr.length; i++) {
+  let nameInString = nameArr[i];
+  // console.log(nameInString);
+
+  let nameFirstLetter = nameInString.substring(0, 1).toLocaleUpperCase();
+  // console.log(nameFirstLetter);
+
+  let nameSecLetter = nameInString.substring(1);
+  // console.log(nameSecLetter);
+
+  let fullName = "";
+  fullName += nameFirstLetter + nameSecLetter;
+  // console.log(fullName);
+
+  let fullNameInArrayForm = fullName.split();
+  console.log(fullNameInArrayForm);
+} /*[ 'Aun' ]
+[ 'Faizan' ]
+[ 'Kashan' ]*/
