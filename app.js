98e28905431b962b1b282e5b632dfe113f6ae71f
Aun-Ali-12 <aunali29331@gmail.com>
2025-05-09 07:58:17 +0500

    my message

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ for (let i = 0; i < nameArr.length; i++) {
   let nameInString = nameArr[i];
   // console.log(nameInString);
 
-  let nameFirstLetter = nameInString.substring(0, 1).toLocaleUpperCase();
+  let nameFirstLetter = nameInString.slice(0, 1).toLocaleUpperCase();
   // console.log(nameFirstLetter);
 
-  let nameSecLetter = nameInString.substring(1);
+  let nameSecLetter = nameInString.slice(1)
   // console.log(nameSecLetter);
 
   let fullName = "";
